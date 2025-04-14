9bb5b62ad3a94fb38fa2c68ddd93b37ff19b6d3a
OmarMosleh <omarmosleh90@gmail.com>
2025-04-14 13:21:11 +0200

    REady for Deployment

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@
 import express from "express";
 // to use the upper syntax go change in the package.json create "type" : "module"
 import dotenv from "dotenv";
+import path from "path"
 dotenv.config();
 // this package was used so we can read the variables in the env file in process.env.MONGO_URI
 import { connectDB } from "./config/db.js";
@@ -10,12 +11,20 @@ import productRoutes from "./routes/product.route.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000
+
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use("/api/products", productRoutes);
-// console.log(process.env.MONGO_URI);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res)=> {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  })
+}
+
 app.listen(PORT, () => {
   connectDB();
   console.log(`server started at http://localhost:${PORT}`);
 });
-// Rkys2QnC5lUNlPlK
-// mongodb+srv://omarmosleh90:Rkys2QnC5lUNlPlK@cluster0.maxsstl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
